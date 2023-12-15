0614d28830b0e9b4d7df017d902fe0e1b2326ad0
Bojan <nikov.bojan@gmail.com>
2023-12-15 15:56:57 +0100

    Initial commit

---
+++ backend/src/main/java/com/example/macedonianlegacy/MacedonianLegacyApplication.java
@@ -0,0 +1,13 @@
+package com.example.macedonianlegacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacedonianLegacyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MacedonianLegacyApplication.class, args);
+    }
+
+}
---
+++ backend/src/main/java/com/example/macedonianlegacy/Model/Location.java
@@ -0,0 +1,11 @@
+//package com.example.macedonianlegacy.Model;
+//
+//import jakarta.persistence.Entity;
+//import lombok.Data;
+//import lombok.RequiredArgsConstructor;
+//
+//@Entity
+//@Data
+//@RequiredArgsConstructor
+//public class Location {
+//}
